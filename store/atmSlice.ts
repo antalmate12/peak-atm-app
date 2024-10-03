97d8f43cb3d314dd7546cd071dce8bb371274521
antalmate12 <antalmate12@gmail.com>
2024-10-03 23:09:18 +0200

    types

--- store/atmSlice.ts
+++ store/atmSlice.ts
@@ -1,6 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+type AllowedBills = '20000' | '10000' | '5000' | '2000';
+
+interface AtmState {
+  bills: Record<AllowedBills, number>;
+  error: string | null;
+}
+
+const initialState: AtmState = {
   bills: {
     20000: 10,
     10000: 20,
