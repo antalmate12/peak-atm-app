2ecc5367fc38b30b29e545661ca9f3daa3127770
antalmate12 <antalmate12@gmail.com>
2024-10-04 13:32:09 +0200

    fix history list

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,19 +1,24 @@
 import { ThemedView } from "@/components/themed/ThemedView";
 import TransactionItem from "@/components/TransactionItem";
+import { Transaction } from "@/store/slices/historySlice";
 import { RootState } from "@/store/store";
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 
 const HistoryScreen = () => {
   const { history } = useSelector((state: RootState) => state.history);
-  console.log(history[0]);
+  const [historyData, setHistoryData] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    setHistoryData([...history].reverse());
+  }, [history]);
 
   return (
     <ThemedView style={styles.container}>
       <FlatList
-        data={history}
-        inverted={true}
+        data={historyData}
         renderItem={({ item }) => <TransactionItem transaction={item} />}
         keyExtractor={(item, index) => index.toString()}
       />
